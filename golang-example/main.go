58a5ebc34923f281ab7a2fa4c60b9e00aef714f9
Power Dancer <xushichangdesmond@gmail.com>
2019-05-21 12:24:57 +0800

    Update main.go

--- golang-example/main.go
+++ golang-example/main.go
@@ -1,82 +1,16 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
-	"errors"
 	"fmt"
-	"math/big"
 	"os"
 )
-
-var (
-	ErrInputSize  = errors.New("input size too large")
-	ErrEncryption = errors.New("encryption error")
-)
-
-func PrivateEncrypt(priv *rsa.PrivateKey, data []byte) (enc []byte, err error) {
-
-	k := (priv.N.BitLen() + 7) / 8
-	tLen := len(data)
-	// rfc2313, section 8:
-	// The length of the data D shall not be more than k-11 octets
-	if tLen > k-11 {
-		err = ErrInputSize
-		return
-	}
-	em := make([]byte, k)
-	em[1] = 1
-	for i := 2; i < k-tLen-1; i++ {
-		em[i] = 0xff
-	}
-	copy(em[k-tLen:k], data)
-	c := new(big.Int).SetBytes(em)
-	if c.Cmp(priv.N) > 0 {
-		err = ErrEncryption
-		return
-	}
-	var m *big.Int
-	var ir *big.Int
-	if priv.Precomputed.Dp == nil {
-		m = new(big.Int).Exp(c, priv.D, priv.N)
-	} else {
-		// We have the precalculated values needed for the CRT.
-		m = new(big.Int).Exp(c, priv.Precomputed.Dp, priv.Primes[0])
-		m2 := new(big.Int).Exp(c, priv.Precomputed.Dq, priv.Primes[1])
-		m.Sub(m, m2)
-		if m.Sign() < 0 {
-			m.Add(m, priv.Primes[0])
-		}
-		m.Mul(m, priv.Precomputed.Qinv)
-		m.Mod(m, priv.Primes[0])
-		m.Mul(m, priv.Primes[1])
-		m.Add(m, m2)
-
-		for i, values := range priv.Precomputed.CRTValues {
-			prime := priv.Primes[2+i]
-			m2.Exp(c, values.Exp, prime)
-			m2.Sub(m2, m)
-			m2.Mul(m2, values.Coeff)
-			m2.Mod(m2, prime)
-			if m2.Sign() < 0 {
-				m2.Add(m2, prime)
-			}
-			m2.Mul(m2, values.R)
-			m.Add(m, m2)
-		}
-	}
-
-	if ir != nil {
-		// Unblind.
-		m.Mul(m, ir)
-		m.Mod(m, priv.N)
-	}
-	enc = m.Bytes()
-	return
-}
 
 func main() {
 	pemString := `-----BEGIN RSA PRIVATE KEY-----
@@ -105,7 +39,14 @@ xgj61LCJMk13kChBAkBwXwAxM5c0qPMbLs2mKDQbqb6KYgcFQZOjsj8u3T6zQvnX
 	toEncrypt := []byte("GET999999999999999")
 	shaDigest := sha256.Sum256(toEncrypt)
 
-	encData, _ := PrivateEncrypt(privKey, shaDigest[:])
-	fmt.Println(base64.StdEncoding.EncodeToString(encData))
+	rng := rand.Reader
+
+	signature, err := rsa.SignPKCS1v15(rng, privKey, crypto.Hash(0), shaDigest[:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error from signing: %s\n", err)
+		return
+	}
+
+	fmt.Println(base64.StdEncoding.EncodeToString(signature))
 
 }
